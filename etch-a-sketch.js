1d5f3f1fca1af3993d915e49ed2f2b57e8dc5871
kieswetter-hub <50299190+kieswetter-hub@users.noreply.github.com>
2023-09-12 16:50:40 +0200

    adds function generateGrid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,7 +2,13 @@
 
 /* create a grid of 16 x 16 using only DOM */
 
-const container = document.getElementById('container');
-const div = document.createElement('div');
-div.classList.add('gridDiv');
-container.appendChild(div);
+function generateGrid (numberOfItems) {
+    let htmlToAdd = "";
+
+    for (let i = 0; i < numberOfItems; i++) {
+        htmlToAdd += `<div class="gridDiv"></div>`;
+    }
+    container.innerHTML = htmlToAdd;
+}
+
+generateGrid(256)
